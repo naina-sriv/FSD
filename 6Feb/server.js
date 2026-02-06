699cdf7a79a4b551168c9386f524bd5c1823862e
naina-sriv <146738328+naina-sriv@users.noreply.github.com>
2026-02-06 11:07:56 +0530

    6.02.2026

---
+++ 6Feb/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    if (req.url=="/"){
+        fs.readFile("index.html",'utf-8',(err,htmlContent)=>{
+            if(err){
+                res.statusCode = 500;
+                res.end('internal server error');
+                return;
+            }
+            res.setHeader('Content-Type','text/html');
+            res.end(htmlContent);
+        });
+    }
+    else if (req.url=="/style.css"){
+        fs.readFile("style.css",'utf-8',(err,cssContent)=>{
+            if(err){
+                res.statusCode = 500;
+                res.end('internal server error');
+                return;
+            }
+            res.setHeader('Content-Type','text/css');
+            res.end(cssContent);
+        }
+        );
+    }  else{
+        res.statusCode = 404;
+        res.end('not found');
+    }
+});
+
+server.listen(3000,()=>{
+    console.log(`server is running on port ${3000}`);
+});
